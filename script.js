e899799e40505485595b56cbd2fe5a7228e13b16
Tazymaru <87786830+Tazymaru@users.noreply.github.com>
2021-09-29 12:28:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondcard;
+let lockBoard = false;
+
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondcard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath() {
+    if(firstCard.dataset.card === secondcard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondcard.removeEventListener('click', flipCard);
+
+    resetboard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondcard.classList.remove('flip');
+
+        resetboard();
+    }, 1500);
+}
+
+function resetboard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondcard] = [null,null];
+}
+
+(function shuffle() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})()
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard);
+});
